66197bc640b57751abcb64d76e43d7d78c11dca2
Samuel VERGNIOL <samuelvgnl@gmail.com>
2019-05-07 15:52:01 +0200

    Alkyys
    Bootstrap + about  1+ home 1

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,52 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div class="container">
+    <div id="app">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Nusdo</a>
+        <button class="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarColor02"
+                aria-controls="navbarColor02"
+                aria-expanded="true"
+                aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="navbar-collapse collapse show" id="navbarColor02" style="">
+          <ul class="navbar-nav mr-auto" id="nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">
+                <router-link to="/">Location</router-link>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                <router-link to="/about">Louer mon vehicule</router-link>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                <router-link to="/about">A propos</router-link>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <router-view/>
     </div>
-    <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+  #nav {
+    a {
+      font-weight: bold;
+      color: #2c3e50;
+
+      &.router-link-exact-active {
+        color: #42b983;
+      }
     }
   }
-}
 </style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,26 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <ul class="nav nav-tabs">
+      <li class="nav-item">
+        <a class="nav-link active" data-toggle="tab" href="#home">Nous somme qui ?</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-toggle="tab" href="#profile">Mention Legal</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-toggle="tab" href="#assurance">Assurance</a>
+      </li>
+    </ul>
+    <div id="myTabContent" class="tab-content">
+      <div class="tab-pane fade active show" id="home">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate doloremque eligendi inventore iste, laboriosam natus numquam pariatur quod ut voluptatem?</p>
+      </div>
+      <div class="tab-pane fade" id="profile">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet aperiam laudantium quis? Itaque, molestiae veritatis? Aliquam earum enim eos id nemo praesentium rem! Doloremque eaque harum ipsa laudantium optio quod sapiente temporibus. A ab beatae dicta doloribus ducimus eaque eveniet expedita explicabo id iste itaque labore, laborum maiores mollitia non nostrum omnis perferendis praesentium quaerat quas ratione, repudiandae saepe temporibus vero vitae voluptatibus voluptatum. Ab commodi minus nihil sed. Est libero mollitia quidem voluptate? Autem corporis labore, natus nemo nihil perspiciatis saepe sit ut voluptatum! Autem, consectetur corporis cum cupiditate dicta dignissimos fuga fugiat hic in inventore ipsa itaque libero nesciunt placeat, porro quam repellat sed, sit ullam velit vero vitae voluptate. Culpa debitis doloribus earum est iusto nulla porro praesentium repellat voluptatem. Autem consequatur corporis iure officiis quos recusandae sapiente. Adipisci architecto, fuga fugit illum incidunt inventore laudantium magnam omnis, provident quis quos rerum sunt tenetur? Alias animi consectetur dignissimos eaque, error exercitationem expedita fugiat ipsa nam nobis officiis quas reprehenderit, rerum sapiente sequi soluta sunt? A animi atque distinctio dolorem ducimus, eaque earum et nam nostrum qui quos sapiente sed voluptatum. At dicta earum facilis minima nisi pariatur porro quaerat voluptatem. Aspernatur dolorem excepturi inventore optio sit. Ducimus.</p>
+      </div>
+      <div class="tab-pane fade" id="assurance">
+        <p>Yolo Yolo Yolo Yolo Yolo Yolo Yolo Yolo Yolo Yolo Yolo Yolo Yolo Yolo Yolo Yolo Yolo Yolo Yolo Yolo Yolo Yolo Yolo Yolo Yolo Yolo </p>
+      </div>
+    </div>
   </div>
 </template>

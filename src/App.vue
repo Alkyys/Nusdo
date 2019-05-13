0fc53a6e68ad9c9e2a4b8c77a5a95de0d0fde4d1
Samuel VERGNIOL <samuelvgnl@gmail.com>
2019-05-13 10:27:43 +0200

    Alkyys
    mise en place des components

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,20 @@
 <template>
   <div class="container">
-    <div id="app">
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">Nusdo</a>
-        <button class="navbar-toggler"
-                type="button"
-                data-toggle="collapse"
-                data-target="#navbarColor02"
-                aria-controls="navbarColor02"
-                aria-expanded="true"
-                aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="navbar-collapse collapse show" id="navbarColor02" style="">
-          <ul class="navbar-nav mr-auto" id="nav">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">
-                <router-link to="/">Location</router-link>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                <router-link to="/about">Louer mon vehicule</router-link>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                <router-link to="/about">A propos</router-link>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-      <router-view/>
-    </div>
+    <navBar/>
+    <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-  #nav {
-    a {
-      font-weight: bold;
-      color: #2c3e50;
+<script>
+import header from './components/header.vue';
 
-      &.router-link-exact-active {
-        color: #42b983;
-      }
-    }
-  }
+export default {
+  components: {
+    navBar: header,
+  },
+};
+
+</script>
+
+<style lang="scss">
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,15 @@
 <template>
-  <div class="home">
-<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+  <div id="app">
+    <!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <div class="card text-white bg-warning mb-3" style="max-width: 20rem;">
+      <div class="card-header">Header</div>
+      <div class="card-body">
+        <h4 class="card-title">Warning card title</h4>
+        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident quibusdam quis
+          repellendus ut. Accusamus aliquam, amet assumenda delectus distinctio dolore eaque esse iste mollitia omnis
+          quae repudiandae suscipit, ut voluptate?</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -14,6 +23,33 @@
 //     HelloWorld,
 //   },
 // };
-
+export default {
+  data() {
+    return {
+      users: [
+        {
+          Nom: 'Carter',
+          Marque: 'Smart',
+          type: 'Break',
+          couleur: 'green',
+          espace_utile: 15,
+          kilometrage: 173016,
+          carburant: 'Diesel',
+          puissance: 106,
+        },
+        {
+          Nom: 'Blair',
+          Marque: 'Ferrari',
+          type: 'Break',
+          couleur: 'indigo',
+          espace_utile: 9,
+          kilometrage: 98113,
+          carburant: 'GPL',
+          puissance: 196,
+        },
+      ],
+    };
+  },
+};
 
 </script>
